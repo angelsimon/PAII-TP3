df501c65a046f484b22132d6f3085c2fd2bd6f82
Angel Simón <angelsimon@hotmail.com.ar>
2020-09-13 22:46:57 -0300

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/grupo06/tp03/RegistrarActivity.java
+++ app/src/main/java/com/grupo06/tp03/RegistrarActivity.java
@@ -3,18 +3,12 @@ package com.grupo06.tp03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class RegistrarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< Updated upstream
-        setContentView(R.layout.activity_main);
-=======
         setContentView(R.layout.activity_registrar);
->>>>>>> Stashed changes
-
     }
 }
